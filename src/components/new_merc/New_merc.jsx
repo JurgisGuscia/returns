64f3d42bb29fd108dfe87d012e071180576b3dd3
JurgisGuscia <j.d.guscia@gmail.com>
2022-10-01 14:48:40 +0300

    finished new merc adding form (no submit functionality yet)

--- src/components/new_merc/New_merc.jsx
+++ src/components/new_merc/New_merc.jsx
@@ -4,9 +4,65 @@ const NewMerc = () => {
   return (
     <div id="newMerc__container" className="newMerc__container hidden">
       <div className="newMerc__container__header">Pridėti naują prekę</div>
-      <div className="newMerc__container__content"></div>
+      <div className="newMerc__container__content">
+        <form id="newMerc-form" className="newMerc__container__content__form">
+          <label className="newMerc__container__content__form__label">
+            Prekės kodas
+          </label>
+          <input
+            className="newMerc__container__content__form__input"
+            type="text"
+          ></input>
+          <label className="newMerc__container__content__form__label">
+            Prekės pavadinimas
+          </label>
+          <input
+            className="newMerc__container__content__form__input"
+            type="text"
+          ></input>
+          <label className="newMerc__container__content__form__label">
+            Kiekis
+          </label>
+          <input
+            className="newMerc__container__content__form__input"
+            type="number"
+          ></input>
+          <label className="newMerc__container__content__form__label">
+            Pirkta
+          </label>
+          <input
+            className="newMerc__container__content__form__input"
+            type="text"
+          ></input>
+          <label className="newMerc__container__content__form__label">
+            Grąžinimo data
+          </label>
+          <input
+            id="time_input"
+            className="newMerc__container__content__form__input"
+            type="date"
+          ></input>
+          <div className="newMerc__container__content__form__button-container">
+            <input
+              className="newMerc__container__content__form__button"
+              type="submit"
+              value="Įterpti"
+            ></input>
+            <input
+              className="newMerc__container__content__form__button"
+              onClick={closeForm}
+              type="button"
+              value="Atšaukti"
+            ></input>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
-
+function closeForm() {
+  document.getElementById("backdrop").classList.add("hidden");
+  document.getElementById("newMerc__container").classList.add("hidden");
+  document.getElementById("newMerc-form").reset();
+}
 export default NewMerc;
